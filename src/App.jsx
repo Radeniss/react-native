070ee17fc452cdc7855be8e08b4af13a542d4b1e
Radeniss~ <radenis354@gmail.com>
2025-06-30 23:15:08 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './Pages/login';
+
+function App() {
+  return (
+  <div className="h-screen flex justify-center items-center">
+    <LoginPage></LoginPage>
+  </div>
+  );
+}
+
+export default App;
