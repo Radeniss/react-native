e5e7632982d37e65b02b7d40553474e016685282
Radeniss~ <radenis354@gmail.com>
2025-07-17 18:54:51 +0700

    update

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,25 +1,30 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
+const Products = [
+    {
+        id: 1,
+        name: "New Car",
+        image: "/images/car-1.png",
+        price: "Rp 1.000.000",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat minus autem rerum numquam accusamus optio hic qui minima. Nulla nobis excepturi quam earum velit error sit laudantium asperiores sapiente totam.",
+    }
+]
+
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-        <CardProduct>
-            <CardProduct.Header image="/images/car-1.png"  />
-            <CardProduct.Body title="New Car">
-                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat minus autem rerum numquam accusamus optio hic qui minima. Nulla nobis excepturi quam earum velit error sit laudantium asperiores sapiente totam.
+        
+       {Products.map((product) => (
+        <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image}  />
+            <CardProduct.Body name={product.title}>
+                 {product.body}
             </CardProduct.Body>
-            <CardProduct.Footer price = "Rp 1.000.000"/>
+            <CardProduct.Footer price={product.price} />
         </CardProduct>
-        <CardProduct>
-            <CardProduct.Header image="/images/car-1.png"  />
-            <CardProduct.Body title="New Car">
-                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat minus autem rerum numquam accusamus optio hic qui minima. Nulla nobis excepturi quam earum velit error sit laudantium asperiores sapiente totam.
-            </CardProduct.Body>
-            <CardProduct.Footer price = "Rp 1.000.000"/>
-        </CardProduct>
+       ))}
         </div>
-
     );
 };
 
