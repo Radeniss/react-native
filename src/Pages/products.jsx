16a565aa18433db7cb8b130e5a9dce44ee56773c
Radeniss~ <radenis354@gmail.com>
2025-07-18 23:24:59 +0700

    update

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
@@ -19,11 +20,23 @@ const Products = [
     }
 ]
 
+const email = localStorage.getItem("email");
+
 const ProductsPage = () => {
+
+    const handleLogout =() => {
+        localStorage.removeItem("email");
+        localStorage.removeItem("password");
+        window.location.href = "/login";}
+
     return (
+       <Fragment>  
+        <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+            {email}
+        <Button className="bg-black ml-5" onClick={handleLogout}>Logout</Button>
+        </div> 
         <div className="flex justify-center py-5">
-        
-       {Products.map((product) => (
+        {Products.map((product) => (
         <CardProduct key={product.id}>
             <CardProduct.Header image={product.image}  />
             <CardProduct.Body name={product.title}> 
@@ -31,8 +44,9 @@ const ProductsPage = () => {
             </CardProduct.Body>
             <CardProduct.Footer price={product.price} />
         </CardProduct>
-       ))}
+         ))}
         </div>
+       </Fragment>
     );
 };
 
--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -5,6 +5,9 @@ const FormLogin = () => {
     const handleLogin = (event) => {
         event.preventDefault();
         console.log ("login");
+        localStorage.setItem("email", event.target.email.value);
+        localStorage.setItem("password", event.target.password.value);
+        window.location.href = "/products";
     }
     return (
         <form onSubmit={handleLogin}>
