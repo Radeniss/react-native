e9800cd9e04e7590a688b5410634536c4e4fe47f
Radeniss~ <radenis354@gmail.com>
2025-07-26 23:10:47 +0700

    update

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -33,7 +33,7 @@ const email = localStorage.getItem("email");
 const ProductsPage = () => {
     const[cart, setCart] = useState([
         {
-            id: "1",
+            id: 1,
             qty: 1,
         }
     ]);
@@ -44,13 +44,19 @@ const ProductsPage = () => {
         window.location.href = "/login";}
 
         const handleAddToCart = (id) => {
+           if(cart.find(item => item.id === id )) {
+            setCart (
+                cart.map(item => item.id === id ? {...item, qty: item.qty + 1} : item)
+            )
+           } else {
             setCart([
                 ...cart,
                 {
-                    id,
-                    qty: 1,
+                    id: id,
+                    qty: 1
                 }
             ])
+           }
         }
 
     return (
@@ -60,7 +66,7 @@ const ProductsPage = () => {
         <Button className="bg-black ml-5" onClick={handleLogout}>Logout</Button>
     </div>
     <div className="flex justify-center py-5">
-        <div className="w-3/4 flex flex-wrap gap-5">
+        <div className="w-3/4 flex flex-wrap gap-2">
             {Products.map((product) => (
                 <CardProduct key={product.id}>
                     <CardProduct.Header image={product.image} />
@@ -75,18 +81,9 @@ const ProductsPage = () => {
                 </CardProduct>
            ))}
         </div>
-        <div className="w-1/4">
-            <h1 className="text-3xl font-bold text-blue-600">
-                cart
-            </h1>
-            <ul>
-                {cart.map((item) => 
-                    (
-                    <li key={item}>{item.id}</li>
-                    )
-                )}
-            </ul>
-            <table>
+        <div className="w-2/6">
+            <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-5">cart</h1>
+            <table className="text-left table-auto border-separate border-spacing-x-5">
                 <thead>
                     <tr>
                         <th>Product</th>
@@ -97,7 +94,15 @@ const ProductsPage = () => {
                 </thead>
                 <tbody>
                     {cart.map((item) => {
-                        const product = products.find((product) => product.id === item.id);
+                        const product = Products.find((product) => product.id === item.id);
+                        return (
+                            <tr key={item.id}>
+                                <td>{product.name}</td>
+                                <td>{product.price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</td>
+                                <td>{item.qty}</td>
+                                <td>{(item.qty * product.price).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</td>
+                            </tr>
+                        )
                     })}
                 </tbody>
             </table>
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
     const { children } = props;
     return (
-        <div className="w-full max-w-sm bg-gray-600 border border-gray-700 rounded-lg shadow   mx-2 my-2 flex flex-col justify-between">
+        <div className="w-full max-w-xs bg-gray-600 border border-gray-700 rounded-lg shadow   mx-2 my-2 flex flex-col justify-between">
             {children}
         </div>
     )
@@ -42,10 +42,10 @@ const Footer = (props) => {
     const { price, handleAddToCart, id } = props;
     return (
         <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">
+            <span className="text-l font-bold text-white">
                 {price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}
             </span>
-            <Button className="bg-blue-600 mr-4" onClick={() => handleAddToCart(id)}>
+            <Button className="bg-blue-600  text-l" onClick={() => handleAddToCart(id)}>
                 Add to Cart
             </Button>
         </div>
